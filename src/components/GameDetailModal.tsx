a79f1a09453d01b4c510c347d662b81e79a13009
Shazzaam <78475878+shazzaam7@users.noreply.github.com>
2026-06-29 14:51:05 +0200

    fix: Fix TS error from renamed map parameter in GameDetailModal

--- src/components/GameDetailModal.tsx
+++ src/components/GameDetailModal.tsx
@@ -535,7 +535,7 @@ export function GameDetailModal({
                     Products ({info.products.related.length})
                   </h3>
                   <div className="space-y-1.5">
-                    {info.products.related.map((url) => (
+                    {info.products.related.map((url, idx) => (
                       <a
                         key={url}
                         href={toHttps(url)}
@@ -556,7 +556,7 @@ export function GameDetailModal({
                             d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"
                           />
                         </svg>
-                        Product {i + 1}
+                        Product {idx + 1}
                       </a>
                     ))}
                   </div>
